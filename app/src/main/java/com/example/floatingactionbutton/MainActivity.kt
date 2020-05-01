106e90b27f360968fce311a00c092c6b44e888b8
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-05-01 17:48:28 +0900

    Log.d("UI_PARTS", "Snackbarをタップした")

--- app/src/main/java/com/example/floatingactionbutton/MainActivity.kt
+++ app/src/main/java/com/example/floatingactionbutton/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.floatingactionbutton
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
